17780279429aa5737ca232c48f1caf9e6d5849b8
Nouredine Diallo <nourredinediallo@gmail.com>
2026-05-25 08:41:39 +0000

    Fix: Ajout de nltk et téléchargement automatique des modèles

--- src/ingestion.py
+++ src/ingestion.py
@@ -15,6 +15,21 @@ from chromadb.config import Settings
 from sentence_transformers import SentenceTransformer
 from nltk.tokenize import sent_tokenize
 import argparse
+import nltk
+import ssl
+
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+# Téléchargement automatique des paquets nltk requis
+nltk.download('punkt', quiet=True)
+nltk.download('punkt_tab', quiet=True)
+nltk.download('averaged_perceptron_tagger', quiet=True)
 
 # Config
 SRC_MAP = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "source_map.json")
